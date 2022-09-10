78778da676c177d09c9622354cf6a49e078f3e3d
kuro <kuroneko961718@gmail.com>
2022-09-10 18:18:24 +0800

    这是第二次修改

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-    echo '现在时间是：'.date('Y-m-d H:i:s', time());
+// echo '现在时间是：'.date('Y-m-d H:i:s', time());
+
+// 这是修改后的提交
+    echo 'Hello world';
---
+++ index.php~
@@ -0,0 +1,3 @@
+<?php
+
+    echo '现在时间是：'.date('Y-m-d H:i:s', time());
